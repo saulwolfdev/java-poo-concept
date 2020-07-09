2ac6b5972c38c928ad0b260abaef4272b73259e1
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2020-07-08 23:33:47 -0300

    main

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,35 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        
+      int uno,dos,tres;
+       
+      uno=332445;
+      System.out.println(uno);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
